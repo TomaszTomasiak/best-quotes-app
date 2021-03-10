6af910aaa79499e3fbe965a92d2fb27ed14d3c3f
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-03-10 11:44:36 +0100

    smart component - added logic to ranking component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Quotation} from './models/quotation';
 import {QUOTES} from './models/database';
 
@@ -11,7 +11,7 @@ export class AppComponent {
   // title = 'best-quotes-app';
   showForm = false;
   quotes: Quotation[] = QUOTES;
-  quotation: Quotation = { author: '', sentence: '', votes: 0} ;
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
 
   onSwitchForm(): void {
     this.showForm = !this.showForm;
@@ -19,10 +19,18 @@ export class AppComponent {
 
   addQuotation() {
     this.quotes.unshift(this.quotation);
-    this.quotation = { author: '', sentence: '', votes: 0} ;
+    this.quotation = {author: '', sentence: '', votes: 0};
   }
 
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
+  }
+
+  bestQuotes() {
+    return this.quotes.filter(q => q.votes > 0);
+  }
+
+  worstQuotes() {
+    return this.quotes.filter(q => q.votes < 0);
   }
 }
