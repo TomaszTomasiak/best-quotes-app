2660344a9a484fda5d0a2a09618e80be587c8be4
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-03-11 11:27:04 +0100

    form component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,8 @@ import {QUOTES} from './models/database';
 })
 export class AppComponent {
   // title = 'best-quotes-app';
-  showForm = false;
+
   quotes: Quotation[] = QUOTES;
-  quotation: Quotation = {author: '', sentence: '', votes: 0};
-
-  onSwitchForm(): void {
-    this.showForm = !this.showForm;
-  }
-
-  addQuotation() {
-    this.quotes.unshift(this.quotation);
-    this.quotation = {author: '', sentence: '', votes: 0};
-  }
 
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
